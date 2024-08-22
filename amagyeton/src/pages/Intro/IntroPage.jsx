99ce73cbea332caa726837637a26e1e67907aee6
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-08-22 10:25:03 +0900

    feat : Intro페이지 초기 화면 설정 완료

---
+++ amagyeton/src/pages/Intro/IntroPage.jsx
@@ -0,0 +1,11 @@
+import SmallBackGroundPage from "../../components/SmallBackground/SmallBackground";
+
+const IntroPage = () => {
+  return (
+    <SmallBackGroundPage>
+      <div>IntroPage</div>
+    </SmallBackGroundPage>
+  );
+};
+
+export default IntroPage;
