83f20c805cb8e9afcffca6fea08ce8c6d40fab1d
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-08-22 10:26:36 +0900

    design : 브라우저 규격으로 크기 설정

---
+++ amagyeton/src/App.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const AppDiv = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
