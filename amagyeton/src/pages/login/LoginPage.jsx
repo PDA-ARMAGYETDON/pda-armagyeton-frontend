dcab5c0c0d3b17f29ff5de1de6e890314908a430
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-08-22 10:25:28 +0900

    feat : 로그인 페이지 초기화면 설정 완료

---
+++ amagyeton/src/pages/login/LoginPage.jsx
@@ -0,0 +1,7 @@
+import SmallBackGroundPage from "../../components/SmallBackground/SmallBackground";
+
+const LoginPage = () => {
+  return <SmallBackGroundPage>LoginPage</SmallBackGroundPage>;
+};
+
+export default LoginPage;
