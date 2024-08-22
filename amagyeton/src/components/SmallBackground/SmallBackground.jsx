62293403d891c23ec53d4cab20c6489895022c36
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-08-22 10:24:05 +0900

    feat : 모바일 뷰 규격 컴포넌트 생성 완료

---
+++ amagyeton/src/components/SmallBackground/SmallBackground.jsx
@@ -0,0 +1,8 @@
+import * as S from "./SmallBackground.style";
+
+// eslint-disable-next-line react/prop-types
+const SmallBackGroundPage = ({ children }) => {
+  return <S.MainDiv>{children}</S.MainDiv>;
+};
+
+export default SmallBackGroundPage;
