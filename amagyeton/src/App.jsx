f82d71f108aa708f5735467eafd767cfb8f8af8b
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-08-23 11:00:28 +0900

    feat : App.jsx에 로그인 , 회원가입 페이지 라우터 추가

--- amagyeton/src/App.jsx
+++ amagyeton/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import * as S from "./App.styles";
 import IntroPage from "./pages/Intro/IntroPage";
 import AccessPage from "./pages/access/AccessPage";
+import SignupPage from "./pages/signup/Signup";
+import LoginPage from "./pages/login/Login";
 
 function App() {
   return (
@@ -9,7 +11,9 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<IntroPage />} />
-          <Route path="/login" element={<AccessPage />} />
+          <Route path="/access" element={<AccessPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
         </Routes>
       </Router>
     </S.AppDiv>
