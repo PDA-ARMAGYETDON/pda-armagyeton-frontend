5891356c3bb324db2b227c0557641003829ff3ea
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-08-23 09:56:35 +0900

    refactor : App.jsx , index.css 코드 수정 , 기능 변경사항 없음

--- amagyeton/src/App.jsx
+++ amagyeton/src/App.jsx
@@ -1,25 +1,25 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { CSSTransition, TransitionGroup } from "react-transition-group";
+// import { CSSTransition, TransitionGroup } from "react-transition-group";
 import * as S from "./App.styles";
 import IntroPage from "./pages/Intro/IntroPage";
-import LoginPage from "./pages/login/LoginPage";
+import AccessPage from "./pages/login/AccessPage";
 
 function App() {
   return (
-    <TransitionGroup>
-      <CSSTransition key={location.key} classNames="page" timeout={300}>
-        <S.AppDiv>
-          <S.AppMobileDiv>
-            <Router>
-              <Routes>
-                <Route path="/" element={<IntroPage />} />
-                <Route path="/login" element={<LoginPage />} />
-              </Routes>
-            </Router>
-          </S.AppMobileDiv>
-        </S.AppDiv>
-      </CSSTransition>
-    </TransitionGroup>
+    // <TransitionGroup>
+    //   <CSSTransition key={location.key} classNames="page" timeout={300}>
+
+    //   </CSSTransition>
+    // </TransitionGroup>
+
+    <S.AppDiv>
+      <Router>
+        <Routes>
+          <Route path="/" element={<IntroPage />} />
+          <Route path="/login" element={<AccessPage />} />
+        </Routes>
+      </Router>
+    </S.AppDiv>
   );
 }
 
--- amagyeton/src/App.styles.js
+++ amagyeton/src/App.styles.js
@@ -4,13 +4,4 @@ export const AppDiv = styled.div`
   width: 100%;
   max-width: 480px;
   min-width: 370px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-export const AppMobileDiv = styled.div`
-  width: 100%;
-  max-width: 480px;
-  height: 100%;
 `;
