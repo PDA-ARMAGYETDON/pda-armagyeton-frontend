c6c289ec266a763e3444a27d6108a8c795054bba
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-08-23 10:33:01 +0900

    refactor : 파일명 변경으로 인해 삭제

--- amagyeton/src/pages/login/LoginPage.jsx
+++
@@ -1,7 +0,0 @@
-import * as S from "./LoginPage.style";
-
-const LoginPage = () => {
-  return <S.MainDiv>login</S.MainDiv>;
-};
-
-export default LoginPage;
--- amagyeton/src/pages/login/LoginPage.style.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const MainDiv = styled.div`
-  width: 100%;
-  margin: 0px auto;
-  background-image: url("/images/background_scroll.png");
-  background-size: cover;
-  position: relative;
-`;
