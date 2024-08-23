e34e38870fc740333ca2defe4446449df1c74538
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-08-23 10:35:22 +0900

    refactor : login 폴더 access로 폴더명 변경

--- amagyeton/src/App.jsx
+++ amagyeton/src/App.jsx
@@ -1,17 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import { CSSTransition, TransitionGroup } from "react-transition-group";
 import * as S from "./App.styles";
 import IntroPage from "./pages/Intro/IntroPage";
-import AccessPage from "./pages/login/AccessPage";
+import AccessPage from "./pages/access/AccessPage";
 
 function App() {
   return (
-    // <TransitionGroup>
-    //   <CSSTransition key={location.key} classNames="page" timeout={300}>
-
-    //   </CSSTransition>
-    // </TransitionGroup>
-
     <S.AppDiv>
       <Router>
         <Routes>
