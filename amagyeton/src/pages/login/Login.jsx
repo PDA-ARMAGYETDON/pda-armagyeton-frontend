4879398ba3f48193c6c190f05b372d8f0ede85e4
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-08-27 08:32:22 +0900

    feat: 로그인 페이지 크기 , app-view로  관리하도록 변경

--- amagyeton/src/pages/login/Login.jsx
+++ amagyeton/src/pages/login/Login.jsx
@@ -1,8 +1,8 @@
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import schema from "./validation.js";
-import * as S from "./Login.style";
-import HeaderNoLogoPage from "../../components/header-no-logo/header-no-logo";
+import LoginUIPage from "./Login.presenter.jsx";
+import AppViewPage from "../../components/app-view/AppView.jsx";
 
 const LoginPage = () => {
   const {
@@ -29,57 +29,19 @@ const LoginPage = () => {
   const errorMessage = getErrorMessage();
 
   return (
-    <S.LoginDiv>
-      <HeaderNoLogoPage />
-      <S.LoginBody>
-        <S.LoginBodyLogo>
-          <img src="/images/logo.png" alt="no image" />
-          <span>아마곗돈</span>
-        </S.LoginBodyLogo>
-        <S.LoginForm onSubmit={handleSubmit(onSubmit)}>
-          <S.LoginIdDiv hasError={!!errors.username}>
-            <label htmlFor="username">아이디</label>
-            <input
-              id="username"
-              type="text"
-              {...register("username", {
-                required: "아이디는 필수 입력입니다.",
-              })}
-              placeholder="아이디를 입력헤주세요"
-            />
-          </S.LoginIdDiv>
-
-          <S.LoginIdDiv hasError={!!errors.password}>
-            <label htmlFor="password">비밀번호</label>
-            <input
-              id="password"
-              type="password"
-              {...register("password", {
-                required: "비밀번호는 필수 입력입니다.",
-              })}
-              placeholder="비밀번호를 입력헤주세요"
-            />
-          </S.LoginIdDiv>
-
-          {errorMessage && (
-            <S.ErrorMessage>
-              <S.ErrorIcon />
-              <span>{errorMessage}</span>
-            </S.ErrorMessage>
-          )}
-
-          <S.SubmitBtnDiv>
-            <S.SubmitBtn
-              type="submit"
-              disabled={!isValid || isSubmitting}
-              isValid={isValid}
-            >
-              로그인
-            </S.SubmitBtn>
-          </S.SubmitBtnDiv>
-        </S.LoginForm>
-      </S.LoginBody>
-    </S.LoginDiv>
+    <AppViewPage
+      children={
+        <LoginUIPage
+          register={register}
+          handleSubmit={handleSubmit}
+          errors={errors}
+          isValid={isValid}
+          isSubmitting={isSubmitting}
+          onSubmit={onSubmit}
+          errorMessage={errorMessage}
+        />
+      }
+    />
   );
 };
 
