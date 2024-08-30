ee704803c329a16dde04306a097aba57e58e90f0
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-08-30 09:36:24 +0900

    feat: 초대장 페이지 경로 설정 완료

--- amagyeton/src/App.jsx
+++ amagyeton/src/App.jsx
@@ -4,18 +4,30 @@ import IntroPage from "./pages/Intro/IntroPage";
 import AccessPage from "./pages/access/AccessPage";
 import SignupPage from "./pages/signup/Signup";
 import LoginPage from "./pages/login/Login";
+import GroupCreatePage from "./pages/group/creaete/GroupCreate";
+import GroupWritePage from "./pages/group/write/GroupWrite";
+import { Provider } from "react-redux";
+import store from "./store/store";
+import InvitePage from "./pages/group/invite/Invite";
 
 function App() {
   return (
     <S.AppDiv>
-      <Router>
-        <Routes>
-          <Route path="/" element={<IntroPage />} />
-          <Route path="/access" element={<AccessPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignupPage />} />
-        </Routes>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<IntroPage />} />
+            <Route path="/access" element={<AccessPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignupPage />} />
+            <Route path="/group">
+              <Route path="create" element={<GroupCreatePage />} />
+              <Route path="write" element={<GroupWritePage />} />
+              <Route path="invite" element={<InvitePage />} />
+            </Route>
+          </Routes>
+        </Router>
+      </Provider>
     </S.AppDiv>
   );
 }
