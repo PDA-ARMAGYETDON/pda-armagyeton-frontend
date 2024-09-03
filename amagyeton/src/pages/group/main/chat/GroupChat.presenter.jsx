e06cb07bcf88a47c311077ab81c960e09888f90e
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-09-04 07:46:51 +0900

    feat: 채팅 페이지 초기 설정

---
+++ amagyeton/src/pages/group/main/chat/GroupChat.presenter.jsx
@@ -0,0 +1,13 @@
+import FooterNav from "../../../../components/footer-nav/FooterNav";
+import HeaderNoLogoPage from "../../../../components/header-no-logo/header-no-logo";
+
+const GroupChatUIPage = () => {
+  return (
+    <>
+      <HeaderNoLogoPage />
+      <FooterNav />
+    </>
+  );
+};
+
+export default GroupChatUIPage;
