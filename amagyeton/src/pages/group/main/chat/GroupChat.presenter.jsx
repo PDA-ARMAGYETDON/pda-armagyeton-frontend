5c613c9c8943625d7dda5e1ccd01626ec8208059
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-09-04 08:43:07 +0900

    feat: 채팅 페이지 메세지 인풋창 구현 완료

--- amagyeton/src/pages/group/main/chat/GroupChat.presenter.jsx
+++ amagyeton/src/pages/group/main/chat/GroupChat.presenter.jsx
@@ -1,11 +1,27 @@
-import FooterNav from "../../../../components/footer-nav/FooterNav";
+import { useState } from "react";
 import HeaderNoLogoPage from "../../../../components/header-no-logo/header-no-logo";
+import * as S from "./GroupChat.style";
 
 const GroupChatUIPage = () => {
+  const [text, setText] = useState("");
+
   return (
     <>
       <HeaderNoLogoPage />
-      <FooterNav />
+      <S.MessageDiv>
+        <S.MessageInput
+          Input
+          type="text"
+          placeholder="메세지를 입력하세요"
+          value={text}
+          onChange={(e) => {
+            setText(e.target.value);
+          }}
+        />
+        <S.Submit>
+          <img src="/images/messageIcon.png" />
+        </S.Submit>
+      </S.MessageDiv>
     </>
   );
 };
