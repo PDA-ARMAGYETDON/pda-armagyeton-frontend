8e2b115b41c60387ee2e9c1b16fc754007854fbc
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-09-03 13:42:28 +0900

    feat: 계좌 개설 페이지 구현 완료

--- amagyeton/src/pages/group/account/Account.jsx
+++ amagyeton/src/pages/group/account/Account.jsx
@@ -1,10 +1,45 @@
+import { useForm } from "react-hook-form";
 import AppViewPage from "../../../components/app-view/AppView";
 import AccountUIPage from "./Account.presenter";
+import { yupResolver } from "@hookform/resolvers/yup";
+import schema from "./validation";
+import { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
 const AccountPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid, isSubmitting },
+  } = useForm({ resolver: yupResolver(schema), mode: "onChange" });
+
+  const [agreeCheck, setAgreeCheck] = useState(false);
+  const navigate = useNavigate();
+  const { id } = useParams();
+
+  const onSubmit = (data) => {
+    if (isValid) {
+      console.log(data);
+      navigate(`/group/${id}/account/complete`);
+    }
+  };
+
+  const onClickCheckAgree = () => {
+    setAgreeCheck((prev) => !prev);
+  };
+
   return (
     <AppViewPage>
-      <AccountUIPage />
+      <AccountUIPage
+        register={register}
+        handleSubmit={handleSubmit}
+        errors={errors}
+        isValid={isValid}
+        onSubmit={onSubmit}
+        isSubmitting={isSubmitting}
+        onClickCheckAgree={onClickCheckAgree}
+        agreeCheck={agreeCheck}
+      />
     </AppViewPage>
   );
 };
