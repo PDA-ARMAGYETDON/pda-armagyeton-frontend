74154657ac4bfcd2c312b91864cca46508a8c5b0
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-09-06 08:13:37 +0900

    feat: 마이페이지 초기세팅 완료

---
+++ amagyeton/src/pages/group/main/mypage/Mypage.jsx
@@ -0,0 +1,12 @@
+import AppViewColorPage from "../../../../components/app-view/AppViewColor";
+import MyPageUIPage from "./Mypage.presenter";
+
+const MyPage = () => {
+  return (
+    <AppViewColorPage>
+      <MyPageUIPage />
+    </AppViewColorPage>
+  );
+};
+
+export default MyPage;
