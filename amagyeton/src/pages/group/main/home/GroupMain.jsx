7ecfb5f354060a8344bec68216e14165eda81faf
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-09-06 14:59:13 +0900

    feat: 모임 홈 페이지 버튼에 함수 추가

--- amagyeton/src/pages/group/main/home/GroupMain.jsx
+++ amagyeton/src/pages/group/main/home/GroupMain.jsx
@@ -1,14 +1,27 @@
 import { useEffect } from "react";
 import AppViewColorPage from "../../../../components/app-view/AppViewColor";
 import GroupMainUIPage from "./GroupMain.presenter";
+import { useNavigate, useParams } from "react-router-dom";
 
 const GroupMainPage = () => {
+  const navigate = useNavigate();
+  const { id } = useParams();
   useEffect(() => {
     localStorage.setItem("activeIndex", 0);
   }, []);
+
+  const onClickGroupRole = () => {
+    navigate(`/group/${id}/groupRole`);
+  };
+  const onClickruleProposal = () => {};
+  const onClickSaleProposal = () => {};
   return (
     <AppViewColorPage>
-      <GroupMainUIPage />
+      <GroupMainUIPage
+        onClickGroupRole={onClickGroupRole}
+        onClickruleProposal={onClickruleProposal}
+        onClickSaleProposal={onClickSaleProposal}
+      />
     </AppViewColorPage>
   );
 };
