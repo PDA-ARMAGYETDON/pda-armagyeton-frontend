cf4bfa887b8e4bed0e8430bc045734e49b02484c
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-09-06 14:59:49 +0900

    feat: 모임원칙 페이지 세팅 완료

---
+++ amagyeton/src/pages/group/main/home/GroupRole/GroupRole.jsx
@@ -0,0 +1,13 @@
+import "react-toastify/dist/ReactToastify.css";
+import AppViewPage from "../../../../../components/app-view/AppView";
+import GroupRoleUIPage from "./GroupRole.presenter";
+
+const GroupRolePage = () => {
+  return (
+    <AppViewPage>
+      <GroupRoleUIPage />
+    </AppViewPage>
+  );
+};
+
+export default GroupRolePage;
