e506771ce9c4fcf271aa9391d0f3aafdba1e1dd6
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-09-08 19:18:09 +0900

    feat:  모임 규칙 보기 페이지 구현 완료

--- amagyeton/src/pages/group/main/home/GroupRole/GroupRole.presenter.jsx
+++ amagyeton/src/pages/group/main/home/GroupRole/GroupRole.presenter.jsx
@@ -1,50 +1,93 @@
 import * as S from "./GroupRole.style";
-import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 import { formatCurrency } from "../../../../../lib/utils/formatCurrency";
 import HeaderGroupPage from "../../../../../components/header-group/header-group";
-import { formatDate } from "../../../../../lib/utils/formatDate";
+import { PendingTeam } from "../../../../../lib/apis/apis";
 
-const data = {
-  name: "에스파는 나야", // 모임명
-  category: "여행", // 카테고리
-  startAt: formatDate("2024-09-01T10:00:00".split("T")[0]), //모임 시작
-  endAt: formatDate("2024-09-01T12:00:00".split("T")[0]), //모임 종료
-  prdyVrssRt: 2.5, // 전일 대비 등락율
-  urgentTradeUpvotes: 5, //긴급 매도 찬성인원
-  tradeUpvotes: 5, // 매매 규칙 찬성인원
-  depositAmt: 10000, // 1인당 기간별 납부금액
-  period: 7, // 납부 주기
-  payDate: "2024-09-15", // 납부 시작일
-  maxLossRt: -5.0, // 해체 규정 최대 손실
-  maxProfitRt: 15.0, // // 해체 규정 최대 수익
-  invitedMembers: 4, // 참가자 인원
-  isLeader: 1, // 리더인지
-  isParticipating: 1, // 참가자인지
-};
+const GroupRoleUIPage = () => {
+  const [teamData, setTeamData] = useState(null);
 
-const role = [
-  { label: "최초 납부금", value: `${formatCurrency(data.depositAmt)}원` },
-  { label: "기간별 납부 금액", value: `${formatCurrency(data.depositAmt)}원` },
-  { label: "납부 시작일", value: formatDate(data.payDate.split("T")[0]) },
-  { label: "납부 주기", value: `${data.isParticipating + 1}일` },
-];
+  useEffect(() => {
+    const fetchTeams = async () => {
+      const res = await PendingTeam();
+      if (res) {
+        console.log(res.data);
+        setTeamData(res.data.data);
+      }
+    };
+    fetchTeams();
+  }, []);
 
-const trading_rules = [
-  { label: "찬성 인원", value: `${data.invitedMembers}명` },
-];
-const urgent_role = [
-  { label: "전일 대비 등락율", value: `-${data.prdyVrssRt}%` },
-  { label: "찬성인원", value: `${data.urgentTradeUpvotes}명` },
-];
+  const translateCategory = (category) => {
+    switch (category) {
+      case "TRAVEL":
+        return "여행";
+      case "HOBBY":
+        return "취미";
+      case "MEAL":
+        return "식비";
+      case "SAVING":
+        return "저축";
+      case "WEDDING":
+        return "결혼자금";
+      case "ETC":
+        return "기타";
+      default:
+        return category;
+    }
+  };
 
-const meeting_period = [{ label: "가입 종료", value: `${data.endAt}` }];
-const dissolution_role = [
-  { label: "최대 수익", value: `${data.maxProfitRt}%` },
-  { label: "최대 손실", value: `${data.maxLossRt}%` },
-];
+  const translatePeriod = (period) => {
+    switch (period) {
+      case "MONTH":
+        return "1달";
+      case "WEEK":
+        return "1주일";
+      default:
+        return period;
+    }
+  };
 
-const GroupRoleUIPage = (props) => {
-  const headCount = useSelector((state) => state.headCount.headCount);
+  const role = [
+    {
+      label: "최초 납부금",
+      value: `${formatCurrency(teamData?.depositAmt)}원`,
+    },
+    {
+      label: "기간별 납부 금액",
+      value: `${formatCurrency(teamData?.depositAmt)}원`,
+    },
+    {
+      label: "납부 시작일",
+      value: teamData?.payDate
+        ? `${teamData.payDate[0]} / ${teamData.payDate[1]} / ${teamData.payDate[2]}`
+        : "",
+    },
+    { label: "납부 주기", value: translatePeriod(teamData?.period) },
+  ];
+
+  const trading_rules = [
+    { label: "찬성 인원", value: `${teamData?.tradeUpvotes}명` },
+  ];
+
+  const urgent_role = [
+    { label: "전일 대비 등락율", value: `-${teamData?.prdyVrssRt}%` },
+    { label: "찬성인원", value: `${teamData?.urgentTradeUpvotes}명` },
+  ];
+
+  const meeting_period = [
+    {
+      label: "가입 종료",
+      value: teamData?.endAt
+        ? `${teamData.endAt[0]}-${teamData.endAt[1]}-${teamData.endAt[2]}`
+        : "",
+    },
+  ];
+
+  const dissolution_role = [
+    { label: "최대 수익", value: `${teamData?.maxProfitRt}%` },
+    { label: "최대 손실", value: `${teamData?.maxLossRt}%` },
+  ];
 
   return (
     <>
@@ -54,19 +97,22 @@ const GroupRoleUIPage = (props) => {
           <S.GroupInfoDiv>
             <S.GroupInfoItem>
               <S.ColorSpan>모임명</S.ColorSpan>
-              <span>{data.name}</span>
+              <span>{teamData?.name}</span>
             </S.GroupInfoItem>
             <S.GroupInfoItem>
               <S.ColorSpan>모임 인원</S.ColorSpan>
-              <span>{data.invitedMembers + 1}명</span>
+              <span>{teamData?.headCount}명</span>
             </S.GroupInfoItem>
             <S.GroupInfoItem>
               <S.ColorSpan>모임 기간</S.ColorSpan>
-              <span>{`${data.startAt} ~ ${data.endAt}`}</span>
+              <span>
+                {teamData?.startAt &&
+                  `${teamData?.startAt[0]} / ${teamData?.startAt[1]} / ${teamData?.startAt[2]} ~ ${teamData?.endAt[0]} / ${teamData?.endAt[1]} / ${teamData?.endAt[2]}`}
+              </span>
             </S.GroupInfoItem>
             <S.GroupInfoItem>
               <S.ColorSpan>카테고리</S.ColorSpan>
-              <S.Category>{data.category}</S.Category>
+              <S.Category>{translateCategory(teamData?.category)}</S.Category>
             </S.GroupInfoItem>
           </S.GroupInfoDiv>
 
@@ -127,14 +173,14 @@ const GroupRoleUIPage = (props) => {
                 {meeting_period.map((rule, index) => (
                   <S.GroupInfoItem key={index}>
                     <span>{rule.label}</span>
-                    <span>{rule.value.split("T")[0]}</span>
+                    <span>{rule.value}</span>
                   </S.GroupInfoItem>
                 ))}
               </S.RoleDiv>
             </S.GroupInfoItem>
           </S.GroupInfoDiv>
 
-          <S.GroupInfoDiv>
+          <S.GroupInfoLastDiv>
             <S.GroupInfoItem>
               <S.LabelSpan>
                 해체 규정 <S.SmallSpan>(전체 수익률)</S.SmallSpan>
@@ -145,46 +191,12 @@ const GroupRoleUIPage = (props) => {
                 {dissolution_role.map((rule, index) => (
                   <S.GroupInfoItem key={index}>
                     <span>{rule.label}</span>
-                    <span>{rule.value.split("T")[0]}</span>
+                    <span>{rule.value}</span>
                   </S.GroupInfoItem>
                 ))}
               </S.RoleDiv>
             </S.GroupInfoItem>
-          </S.GroupInfoDiv>
-
-          <S.ParticipantDiv>
-            <S.LabelSpan>참가자</S.LabelSpan>
-            <div>
-              {new Array(headCount - 1).fill("").map((_, i) => {
-                const isActive = i < data.invitedMembers;
-                return <S.Participant key={i} isActive={isActive} />;
-              })}
-            </div>
-            {headCount - 1 - data.invitedMembers !== 0 && (
-              <S.AlarmSpan>{`${
-                headCount - 1 - data.invitedMembers
-              }명의 인원이 더 침기히면 모임 생성이 가능해요`}</S.AlarmSpan>
-            )}
-          </S.ParticipantDiv>
-          {data.isLeader === 1 && (
-            <S.ShareBtn
-              isCheck={headCount - 1 !== data.invitedMembers}
-              onClick={props.onShareInviteCode}
-            >
-              <S.CustomShareIcon /> <span>초대장 공유하기</span>
-            </S.ShareBtn>
-          )}
-
-          {data.isLeader === 1 ? (
-            <S.SubmitBtn
-              isCheck={headCount - 1 === data.invitedMembers}
-              onClick={props.onClickAccount}
-            >
-              모임 생성하기
-            </S.SubmitBtn>
-          ) : (
-            <S.SubmitBtn isCheck={true}>참가하기</S.SubmitBtn>
-          )}
+          </S.GroupInfoLastDiv>
         </S.GroupInfoSection>
       </S.PendingDiv>
       <S.CustomToastContainer position="bottom-center" autoClose={3000} />
