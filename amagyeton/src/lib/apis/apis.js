24c66ebc52c5ee2ab2b376931630557ce8be8dfb
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-09-09 07:42:26 +0900

    feat: 모임생성 및 규칙 제안 api 추가

--- amagyeton/src/lib/apis/apis.js
+++ amagyeton/src/lib/apis/apis.js
@@ -4,9 +4,10 @@ import axiosInstance from "./axiosInstance";
 //const API_BASE_URL = ""; // 서버의 기본 URL로 변경하세요.
 
 export const registerUser = async (userData) => {
+  console.log(userData);
   try {
     const response = await axios.post(
-      `http://localhost:8081/api/users/signup`,
+      `http://localhost:8080/api/users/signup`,
       userData
     );
     return response.data;
@@ -19,7 +20,7 @@ export const LoginUser = async (userData) => {
   console.log(userData);
   try {
     const response = await axios.post(
-      "http://localhost:8081/api/users/login",
+      "http://localhost:8080/api/users/login",
       userData,
       {
         headers: {
@@ -48,7 +49,7 @@ export const createGroup = async (data) => {
 export const participationGroup = async (id) => {
   try {
     const response = await axiosInstance.get(`/teams/${id}/participate`);
-    return response;
+    return response.data;
   } catch (error) {
     console.error(error);
     return null;
@@ -65,10 +66,34 @@ export const UserTeams = async () => {
   }
 };
 
-export const PendingTeam = async () => {
+export const InviteCode = async (code) => {
   try {
-    const response = await axiosInstance.get(`/teams/pending`);
-    return response;
+    const response = await axiosInstance.get(`/teams`, {
+      params: {
+        inviteCode: code,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
+export const PendingTeam = async (id) => {
+  try {
+    const response = await axiosInstance.get(`/teams/${id}/pending`);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
+export const CreateTeam = async () => {
+  try {
+    const response = await axiosInstance.put(`/teams`);
+    return response.data;
   } catch (error) {
     console.error(error);
     return null;
@@ -99,6 +124,31 @@ export const RoleVoteSuggest = async (id, data) => {
   console.log(id, data);
   try {
     const response = await axiosInstance.post(`/groups/${id}/rules`, data);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
+export const CheckId = async (id) => {
+  console.log(id);
+  try {
+    const response = await axiosInstance.post(`/users/valid/id`, {
+      loginId: id,
+    });
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
+export const CheckEmail = async (email) => {
+  try {
+    const response = await axiosInstance.post(`/users/valid/email`, {
+      email: email,
+    });
     return response.data;
   } catch (error) {
     console.error(error);
