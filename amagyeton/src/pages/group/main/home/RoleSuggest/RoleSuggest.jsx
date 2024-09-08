58de1e717d5a74d0601efecc64b9a4f546919398
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-09-08 09:37:50 +0900

    feat: 규칙제안 페이지 구현중

---
+++ amagyeton/src/pages/group/main/home/RoleSuggest/RoleSuggest.jsx
@@ -0,0 +1,12 @@
+import AppViewColorPage from "../../../../../components/app-view/AppViewColor";
+import RoleSuggestUIPage from "./RoleSuggest.presenter";
+
+const RoleSuggestPage = () => {
+  return (
+    <AppViewColorPage>
+      <RoleSuggestUIPage />
+    </AppViewColorPage>
+  );
+};
+
+export default RoleSuggestPage;
