bc402145116f720ad4f21e0efc64b7a331ccbddc
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-09-08 19:18:58 +0900

    feat: 규칙 제안하기 페이지 구현 완료

--- amagyeton/src/pages/group/main/home/RoleSuggest/RoleSuggest.jsx
+++ amagyeton/src/pages/group/main/home/RoleSuggest/RoleSuggest.jsx
@@ -1,10 +1,86 @@
+import { useEffect, useState } from "react";
 import AppViewColorPage from "../../../../../components/app-view/AppViewColor";
 import RoleSuggestUIPage from "./RoleSuggest.presenter";
+import {
+  GroupRole,
+  PendingTeam,
+  RoleSuggest,
+} from "../../../../../lib/apis/apis";
+import { useParams } from "react-router-dom";
+import RoleSuggestModal from "../../../../../components/role-suggest-modal/roleSuggestModal";
 
 const RoleSuggestPage = () => {
+  const { id } = useParams();
+  const [isOpen, setIsOpen] = useState(false);
+  const [modalContent, setModalContent] = useState(null);
+  const [groupRole, setGroupRole] = useState();
+  const [groupInfo, setGroupInfo] = useState();
+  const [roleData, setRoleData] = useState({
+    payFeeOffers: [],
+    urgentSaleOffers: [],
+    upvoteNumberOffers: [],
+    disbandOffers: [],
+  });
+
+  useEffect(() => {
+    const fetchGroupInfo = async () => {
+      const res = await PendingTeam();
+      setGroupInfo(res.data);
+    };
+    fetchGroupInfo();
+  }, []);
+
+  useEffect(() => {
+    const fetchGroupRole = async () => {
+      const res = await GroupRole();
+      setGroupRole(res.data);
+    };
+    fetchGroupRole();
+  }, []);
+
+  useEffect(() => {
+    const fetchSuggest = async () => {
+      const res = await RoleSuggest(id);
+      console.log(res);
+      setRoleData(res.data);
+    };
+    fetchSuggest();
+  }, []);
+
+  const onClickMoveToCheckInvite = (title, content) => () => {
+    setModalContent({
+      title,
+      type: content.map((rule) => ({
+        ...rule,
+        type: rule.type,
+      })),
+    });
+    setIsOpen(true);
+  };
+
+  const handlePartiModalClose = () => {
+    console.log("close");
+    setIsOpen(false);
+    setModalContent(null);
+  };
+
   return (
     <AppViewColorPage>
-      <RoleSuggestUIPage />
+      <RoleSuggestUIPage
+        onClickMoveToCheckInvite={onClickMoveToCheckInvite}
+        roleData={roleData}
+        groupRole={groupRole}
+        groupInfo={groupInfo}
+      />
+      {isOpen && (
+        <RoleSuggestModal
+          isOpen={isOpen}
+          onClose={handlePartiModalClose}
+          content={modalContent}
+          groupRole={groupRole}
+          groupInfo={groupInfo}
+        />
+      )}
     </AppViewColorPage>
   );
 };
--- amagyeton/src/pages/group/main/home/RoleSuggest/RoleSuggest.presenter.jsx
+++ amagyeton/src/pages/group/main/home/RoleSuggest/RoleSuggest.presenter.jsx
@@ -1,19 +1,29 @@
-import { useEffect, useState } from "react";
-import Slider from "react-slick"; // Slider library example
-import HeaderAlarmPage from "../../../../../components/header-alarm/header-alarm";
+import { useState } from "react";
+import Slider from "react-slick";
+import styled from "styled-components"; // Import styled from styled-components
+import HeaderGroupPage from "../../../../../components/header-group/header-group";
+import FooterNav from "../../../../../components/footer-nav/FooterNav";
+
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import * as S from "./RoleSuggest.style";
+import { formatCurrency } from "../../../../../lib/utils/formatCurrency";
 
-const RoleSuggestUIPage = () => {
+const RedText = styled.span`
+  color: #d40101;
+  font-weight: 600;
+`;
+
+const RoleSuggestUIPage = ({
+  roleData,
+  onClickMoveToCheckInvite,
+  groupRole,
+  groupInfo,
+}) => {
   const [currentSlide, setCurrentSlide] = useState(0);
   const [curSuggest, setCurSuggest] = useState(null);
-  const [roleData, setRoleData] = useState({
-    payFeeOffers: [],
-    urgentSaleOffers: [],
-    upvoteNumberOffers: [],
-    disbandOffers: [],
-  });
+
+  console.log(groupInfo);
 
   const settings = {
     dots: true,
@@ -23,159 +33,182 @@ const RoleSuggestUIPage = () => {
     slidesToScroll: 1,
     afterChange: (index) => {
       setCurrentSlide(index);
-      setCurSuggest(null); // Reset the selected suggestion when the slide changes
+      setCurSuggest(index);
     },
     arrows: false,
   };
-
-  // Simulate fetching data from an API
-  useEffect(() => {
-    // Mocked API data
-    const fetchData = async () => {
-      const response = {
-        code: 0,
-        success: true,
-        message: "string",
-        data: {
-          payFeeOffers: [
-            {
-              type: "모든 멤버는 매달 20일마다 600,000원 납부",
-              upvotes: 2,
-              downvotes: 0,
-              totalvotes: 5,
-              depositAmt: 0,
-              period: "WEEK",
-              payDate: "2024-09-08",
-            },
-          ],
-          urgentSaleOffers: [
-            {
-              type: "모든 멤버는 매달 20일마다 600,000원 납부",
-              upvotes: 2,
-              downvotes: 0,
-              totalvotes: 5,
-              tradeUpvotes: 0,
-              prdyVrssRt: 0,
-            },
-          ],
-          upvoteNumberOffers: [
-            {
-              type: "모든 멤버는 매달 20일마다 600,000원 납부",
-              upvotes: 2,
-              downvotes: 0,
-              totalvotes: 5,
-              tradeUpvotes: 0,
-            },
-          ],
-          disbandOffers: [
-            {
-              type: "모든 멤버는 매달 20일마다 600,000원 납부",
-              upvotes: 2,
-              downvotes: 0,
-              totalvotes: 5,
-              maxLossRt: 0,
-              maxProfitRt: 0,
-            },
-          ],
-        },
-      };
-
-      // Set fetched data to state
-      setRoleData(response.data);
-    };
-
-    fetchData();
-  }, []);
 
   const handleSuggest = (index) => () => {
     setCurSuggest((prev) => (prev === index ? null : index));
   };
 
   const handleAgree = (event) => {
-    event.stopPropagation(); // Prevent the suggestion from being toggled off
-    console.log("Agree clicked for suggestion", curSuggest);
+    event.stopPropagation();
   };
 
   const handleDisagree = (event) => {
-    event.stopPropagation(); // Prevent the suggestion from being toggled off
-    console.log("Disagree clicked for suggestion", curSuggest);
+    event.stopPropagation();
   };
 
-  const renderContent = () => {
-    const sections = [
-      { title: "납부규칙", data: roleData.payFeeOffers },
-      { title: "긴급 매도규칙", data: roleData.urgentSaleOffers },
-      { title: "매매규칙", data: roleData.upvoteNumberOffers },
-      { title: "모임 해체 규칙", data: roleData.disbandOffers },
-    ];
+  const sections = [
+    {
+      title: "납부규칙",
+      data: (
+        <>
+          모든 멤버는{" "}
+          <RedText>{groupRole?.period === "WEEK" ? "1주일" : "매달"}</RedText>
+          마다 <RedText>{formatCurrency(groupRole?.depositAmt)}</RedText>원을
+          납부해야 합니다.
+        </>
+      ),
+      suggestions: roleData?.payFeeOffers || [],
+    },
+    {
+      title: "긴급 매도규칙",
+      data: (
+        <>
+          ※긴급 상황: 전일 종가 대비 <RedText>{groupRole?.prdyVrssRt}%</RedText>
+          등락한 경우
+          <br />
+          각 종목에 대해 긴급 상황의 경우,
+          <br />
+          <RedText>{groupRole?.urgentTradeUpvotes}명</RedText>이 찬성하는 경우에
+          매매 제안이 수락되어 매도 주문됨.
+        </>
+      ),
+      suggestions: roleData?.urgentSaleOffers || [],
+    },
+    {
+      title: "매매규칙",
+      data: (
+        <>
+          매매를 제안할 때 <RedText>{groupRole?.tradeUpvotes}명</RedText> 이상의
+          찬성표를 받아야 매매 주문됨.
+        </>
+      ),
+      suggestions: roleData?.upvoteNumberOffers || [],
+    },
+    {
+      title: "모임 해체 규칙",
+      data: (
+        <>
+          전체 수익률이 <RedText>최대 {groupRole?.maxProfitRt}%</RedText> 달성,
+          <RedText>최소 {groupRole?.maxLossRt}%</RedText> 이하인 경우 자동으로
+          모임이 해체됨.
+        </>
+      ),
+      suggestions: roleData?.disbandOffers || [],
+    },
+  ];
 
-    return sections.map((section, index) => (
+  const renderContent = () =>
+    sections.map((section, index) => (
       <S.RoleDiv key={index}>
         <label>{section.title}</label>
-        {section.data.map((item, i) => (
-          <div key={i}>
-            <div>모든 맴버는 매달 15일마다 500,000원 납부함.</div>
-            <div></div>
-          </div>
-        ))}
+        <div>{section.data}</div>
       </S.RoleDiv>
     ));
+
+  const renderSuggestionText = (suggestion) => {
+    switch (suggestion.type) {
+      case "DISBAND":
+        return (
+          <>
+            전체 수익률이 최대 <RedText>{suggestion.maxProfitRt}%</RedText>{" "}
+            달성, <RedText>{suggestion.maxLossRt}%</RedText> 이하인 경우
+            자동으로 모임이 해체됨.
+          </>
+        );
+      case "PAY_FEE":
+        return (
+          <>
+            모든 멤버는
+            <RedText>{formatCurrency(suggestion.depositAmt)}</RedText>원을{" "}
+            <RedText>{suggestion.period === "WEEK" ? "1주일" : "매달"}</RedText>
+            마다 납부해야 합니다.
+          </>
+        );
+      case "URGENT_SALE":
+        return (
+          <>
+            전일 종가 대비 <RedText>{suggestion.prdyVrssRt}%</RedText> 등락한
+            경우, <RedText>{suggestion.tradeUpvotes}명</RedText>이 찬성하는 경우
+            매도 주문됨.
+          </>
+        );
+      case "UPVOTE_NUMBER":
+        return (
+          <>
+            매매 제안은 <RedText>{suggestion.tradeUpvotes}명</RedText> 이상의
+            찬성표를 받아야 매매 주문됨.
+          </>
+        );
+      default:
+        return "제안 내용이 없습니다.";
+    }
   };
 
   const renderSuggestions = () => {
-    const suggestionsData = [
-      roleData.payFeeOffers,
-      roleData.urgentSaleOffers,
-      roleData.upvoteNumberOffers,
-      roleData.disbandOffers,
-    ];
-
-    const currentSuggestions = suggestionsData[currentSlide] || [];
-
+    const currentSection = sections[currentSlide];
+    const currentSuggestions = currentSection?.suggestions || [];
+    console.log(currentSuggestions);
     return (
       <>
-        <S.AddRoleSuggest onClick={onClickAddSuggest}>
+        <S.AddRoleSuggest
+          onClick={onClickMoveToCheckInvite(
+            currentSection?.title,
+            currentSuggestions
+          )}
+        >
           <S.PlusIcon />
         </S.AddRoleSuggest>
-        {currentSuggestions.map((suggestion, i) => (
-          <S.RoleInfoItem
-            key={i}
-            onClick={handleSuggest(i)}
-            isCheck={curSuggest === i}
-          >
-            <span>{suggestion.type}</span>
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "space-between",
-                marginTop: "15px",
-              }}
+
+        {currentSuggestions.length > 0 ? (
+          currentSuggestions.map((suggestion, i) => (
+            <S.RoleInfoItem
+              key={i}
+              onClick={handleSuggest(i)}
+              isCheck={curSuggest === i}
             >
-              <S.Voter>홍길동</S.Voter>
-              <div>
-                {new Array(suggestion.totalvotes).fill("").map((_, i) => {
-                  const isActive = i < suggestion.upvotes;
-                  return <S.Participant key={i} isActive={isActive} />;
-                })}
+              <span>{renderSuggestionText(suggestion)}</span>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  marginTop: "15px",
+                }}
+              >
+                <S.Voter>홍길동</S.Voter>
+                <div>
+                  {new Array(suggestion.totalvotes).fill("").map((_, i) => {
+                    const isActive = i < suggestion.upvotes;
+                    return <S.Participant key={i} isActive={isActive} />;
+                  })}
+                </div>
               </div>
-            </div>
-            {curSuggest === i && (
-              <S.ButtonGroup>
-                <S.AgreeButton onClick={handleAgree}>찬성</S.AgreeButton>
-                <S.DisagreeButton onClick={handleDisagree}>
-                  반대
-                </S.DisagreeButton>
-              </S.ButtonGroup>
-            )}
-          </S.RoleInfoItem>
-        ))}
+              {curSuggest === i && (
+                <S.ButtonGroup>
+                  <S.AgreeButton onClick={handleAgree}>찬성</S.AgreeButton>
+                  <S.DisagreeButton onClick={handleDisagree}>
+                    반대
+                  </S.DisagreeButton>
+                </S.ButtonGroup>
+              )}
+            </S.RoleInfoItem>
+          ))
+        ) : (
+          <S.NonSuggest>
+            <S.NonIcon />
+            <div>모임원이 제안한 규칙이 없어요</div>
+          </S.NonSuggest>
+        )}
       </>
     );
   };
 
   return (
     <>
-      <HeaderAlarmPage />
+      <HeaderGroupPage />
       <S.Section>
         <S.Label>
           <label>규칙 제안</label>
@@ -190,6 +223,7 @@ const RoleSuggestUIPage = () => {
           </div>
         </S.SectionItem>
       </S.Section>
+      <FooterNav />
     </>
   );
 };
