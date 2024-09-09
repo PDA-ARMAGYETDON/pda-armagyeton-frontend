adfc822fbf7cafbdfeb20369eeec46bf9479aa89
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-09-09 11:45:53 +0900

    feat: 개인 계좌 성공 페이지 추가

---
+++ amagyeton/src/pages/group/account-success-personal/Account.personal.style.js
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+
+export const AccountSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`;
+
+export const TextDiv = styled.div``;
+
+export const ColorSpan = styled.span`
+  color: #3f8cff;
+`;
+
+export const BigText = styled.span`
+  display: block;
+  font-size: 1.5rem;
+  font-weight: 600;
+  margin: 10px 0px;
+`;
+
+export const SmallText = styled.span`
+  display: block;
+  font-size: 0.9rem;
+`;
+
+export const AccountItemDiv = styled.div`
+  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  & img {
+    width: 80%;
+  }
+`;
+
+export const CompleteBtn = styled.button`
+  width: 70%;
+  background-color: #195efd;
+  color: white;
+  font-size: 0.9rem;
+  border: none;
+  border-radius: 12px;
+  padding: 9px 20px;
+  margin-top: 10px;
+`;
+
+export const ErrorSpan = styled.span`
+  color: ${(props) => (props.isAgree ? "red" : "")};
+`;
---
+++ amagyeton/src/pages/group/account-success-personal/Account.personal.success.presenter.jsx
@@ -0,0 +1,25 @@
+import HeaderPage from "../../../components/header/header";
+import * as S from "./Account.personal.style";
+
+const AccountPersonalSuccessUIPage = (props) => {
+  return (
+    <>
+      <HeaderPage></HeaderPage>
+      <S.AccountSection>
+        <div>
+          <S.BigText>
+            나의 <S.ColorSpan>증권 개인 계좌</S.ColorSpan>가<br />
+            개설되었습니다!
+          </S.BigText>
+          <S.SmallText>이제 나의 모임을 생성 할 수 있어요</S.SmallText>
+        </div>
+        <S.AccountItemDiv>
+          <img src="/images/account.png" />
+          <S.CompleteBtn onClick={props.onClickMoveToHome}>확인</S.CompleteBtn>
+        </S.AccountItemDiv>
+      </S.AccountSection>
+    </>
+  );
+};
+
+export default AccountPersonalSuccessUIPage;
