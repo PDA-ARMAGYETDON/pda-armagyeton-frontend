418dcdb9306a32f8e6fe1bd92cc6644c652d107b
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-09-10 16:59:29 +0900

    feat: 포트폴리오 상세 페이지 구현 완료

---
+++ amagyeton/src/pages/group/main/home/StockDetail/StockDetail.jsx
@@ -0,0 +1,7 @@
+import AppViewColorPage from "../../../../../components/app-view/AppViewColor";
+
+const StockDetailPage = () => {
+  return <AppViewColorPage></AppViewColorPage>;
+};
+
+export default StockDetailPage;
