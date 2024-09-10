cd4c7adf320f0bc9855f950460a29ced30750e09
JaeIn1 <97165077+JaeIn1@users.noreply.github.com>
2024-09-10 23:51:48 +0900

    feat: 인기 종목 리스트 변경

--- amagyeton/src/pages/group/main/stocks/search/Stock.search.presenter.jsx
+++ amagyeton/src/pages/group/main/stocks/search/Stock.search.presenter.jsx
@@ -1,29 +1,32 @@
+import { useNavigate, useParams } from "react-router-dom";
 import HeaderStockSearchPage from "../../../../../components/header-stock-search/header-stock-search";
 import * as S from "./Stock.search.style";
 
 const StockSearchUIPage = () => {
+  const navigate = useNavigate();
+  const { id } = useParams();
+
   const data = [
-    {
-      name: "삼성전자",
-      rate: -1.3,
-    },
-    {
-      name: "네이버",
-      rate: -1.3,
-    },
-    {
-      name: "카카오",
-      rate: +2.6,
-    },
-    {
-      name: "sk하이닉스",
-      rate: -1.3,
-    },
-    {
-      name: "HLB",
-      rate: 3.9,
-    },
+    { name: "삼성전자", rate: -1.3, code: "005930" },
+    { name: "네이버", rate: -1.3, code: "035420" },
+    { name: "카카오", rate: +2.6, code: "035720" },
+    { name: "sk하이닉스", rate: -1.3, code: "000660" },
+    { name: "HLB", rate: 3.9, code: "028300" },
+    { name: "LG에너지솔루션", rate: 1.2, code: "373220" },
+    { name: "삼성바이오로직스", rate: -0.5, code: "207940" },
+    { name: "현대차", rate: 0.8, code: "005380" },
+    { name: "셀트리온", rate: -2.1, code: "068270" },
+    { name: "기아", rate: 1.9, code: "000270" },
+    { name: "KB금융", rate: -0.4, code: "105560" },
+    { name: "신한지주", rate: 0.2, code: "055550" },
   ];
+
+  // 종목 클릭 시 해당 종목 코드로 URL 변경 함수
+  const handleStockClick = (code) => {
+    console.log(code);
+    navigate(`/group/${id}/stocks/${code}`);
+  };
+
   return (
     <>
       <HeaderStockSearchPage />
@@ -35,10 +38,12 @@ const StockSearchUIPage = () => {
         <div style={{ width: "100%", marginTop: "30px" }}>
           {data.map((e, i) => {
             return (
-              <S.RankList key={i}>
+              <S.RankList key={i} onClick={() => handleStockClick(e.code)}>
                 <S.RankListLeft>
-                  <span>{i + 1}</span>
-                  <span>{e.name}</span>
+                  <span style={{ color: "black" }}>{i + 1}</span>
+                  <span style={{ cursor: "pointer", color: "black" }}>
+                    {e.name}
+                  </span>
                 </S.RankListLeft>
                 <S.RankListRight isCheck={e.rate < 0}>
                   {e.rate}%
